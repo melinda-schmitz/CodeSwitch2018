8e0263e86238cf987a489bc013aa3924dbd4d97e
Melinda Schmitz <melindaschmitz@chsinc.com>
2018-09-22 17:59:15 -0500

    Added basic NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@
 import React, { Component } from 'react';
 // Import App css file
 import './App.css';
+import NavBar from './components/NavBar';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-
+				<NavBar />
 			</div>
 		);
 	}
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-class NavBar extends Component {
-	render() {
-		return (
-			<div>
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
-			</div>
-		)
-	}
+function NavBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(NavBar);

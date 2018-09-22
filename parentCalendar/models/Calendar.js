090b79b0cde0581858758015fbcfcfd7752adf7b
unknown <bubbastinx@gmail.com>
2018-09-22 15:43:34 -0500

    created models for the parentCalendar

---
+++ parentCalendar/models/Calendar.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CalendarSchema = new Schema ({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  events: {
+    type: Schema.Types.ObjectId,
+    ref: 'events'
+  }
+
+});
+
+module.exports = Calendar = mongoose.model('calendar', PostSchema);

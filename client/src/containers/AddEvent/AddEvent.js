ac914ad812522dbebe1a85743e99d032573a490d
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-22 18:05:38 -0500

    added pages

---
+++ client/src/containers/AddEvent/AddEvent.js
@@ -0,0 +1,14 @@
+// import React
+import React, { Component } from 'react'
+
+class AddEvent extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Add Event</h1>
+			</div>
+		)
+	}
+}
+
+export default AddEvent;

1b9f320811c16b86074ffe9d4396192b2884217d
Melinda Schmitz <melindaschmitz@chsinc.com>
2018-09-22 20:46:48 -0500

    Created Files

---
+++ client/src/containers/EventDetails/EventDetails.js
@@ -0,0 +1,14 @@
+// import React
+import React, { Component } from 'react'
+
+class EventDetails extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Event Details</h1>
+			</div>
+		)
+	}
+}
+
+export default EventDetails;

536d67a946400cbea0d9674336ff79f8812ca817
Melinda Schmitz <melindaschmitz@chsinc.com>
2018-09-22 19:44:29 -0500

    Switched to Class

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = {
   root: {
@@ -18,25 +20,48 @@ const styles = {
     marginRight: 20
   },
   loginButton: {
-	fontSize: '1.5em'
+    fontSize: '1.5em'
   }
 };
 
-function NavBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit"><i class="fas fa-bars"></i></Button>
-		  <Typography variant="title" color="inherit" className={classes.grow}>
-            Edu Stuff To Do
-          </Typography>
-          <Button color="inherit" className={classes.loginButton}><i class="far fa-user-circle"></i></Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class NavBar extends Component{
+  constructor() {
+    super();
+    this.state = {open: false};
+  }
+  
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+  
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit" className={classes.menuButton} onClick={this.toggleDrawer('left', true)}>
+              <i class="fas fa-bars" />
+            </Button>
+            <Typography variant="title" color="inherit" className={classes.grow}>
+              Edu Stuff To Do
+            </Typography>
+            <Button color="inherit" className={classes.loginButton}>
+              <i class="far fa-user-circle" />
+            </Button>
+          </Toolbar>
+        </AppBar>
+    
+        <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)} containerStyle={styles.navBar}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+
+  }
 }
 
 NavBar.propTypes = {
